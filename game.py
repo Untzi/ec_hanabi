5c328c7cc8dc48136651c881be1522e67f1fc15c
BGU-USERS\shwarzs <shwarzs@post.bgu.ac.il>
2018-12-29 16:41:37 +0200

    game start and classes

---
+++ game.py
@@ -0,0 +1,51 @@
+from random import shuffle
+colors = ['blue', 'red', 'yellow', 'white', 'green']
+val_count = {1: 3, 2: 2, 3: 2, 4: 2, 5: 1}
+known_status = ['color', 'val', 'color_and_val', 'none']
+
+class Card:
+
+    def __init__(self, color, val):
+        self.val = val
+        self.color = color
+
+    def __str__(self):
+        return self.color + '-' + str(self.val)
+
+class Player:
+    def __init__(self,player_num,hand):
+        self.player_num = player_num
+        self.hand = [{'card': card, 'status': 'none'} for card in hand]
+
+
+class Game:
+    def __init__(self, players_num):
+        self.deck = self.create_deck()
+        self.discards = []
+        self.clues = 8
+        self.strikes = 3
+        self.turn = 0
+        self.players = []
+        self.inPlay = {color: 0 for color in colors}
+        for player in range(players_num):
+            hand = []
+            for i in range(5):
+                hand.append(self.deck.pop())
+            self.players.append(Player(player, hand))
+
+    def next_turn(self):
+        self.turn = (self.turn+1) % len(self.players)
+        return self.turn
+
+    @staticmethod
+    def create_deck():
+        deck = []
+        for color in colors:
+            for val, count in val_count.items():
+                cards = [Card(color, val) for i in range(count)]
+                deck = deck + cards
+        shuffle(deck)
+        return deck
+
+if __name__=='__main__':
+    game = Game(2)
